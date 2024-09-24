86206faef678e609b66749d6778425a61dd434cb
DESKTOP-89LA26K\vidma <vidmantaszuk@gmail.com>
2024-09-24 18:26:19 +0300

    room list

---
+++ BattleShips/BattleShips/Models/Room.cs
@@ -0,0 +1,11 @@
+﻿namespace BattleShips.Models
+{
+    public class Room
+    {
+        public int ID { get; set; }
+        public string RoomName { get; set; }
+        public int MaxPlayerCount { get; set; }
+        public int CurrentPlayerCount { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ BattleShips/BattleShips/Pages/Index.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using BattleShips.Models;
+
+namespace BattleShips.Pages
+{
+    public partial class Index
+    {
+        public List<Room> GetAllRoom()
+        {
+            List<Room> list = new List<Room>();
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(
+                    new Room
+                    {
+                        ID = i,
+                        RoomName = "testas " + i.ToString(),
+                        MaxPlayerCount = 3,
+                        CurrentPlayerCount = rnd.Next(0,4),
+                        DateCreated = DateTime.Now
+                    }
+                    );
+            }
+            return list;
+        }
+    }
+}
