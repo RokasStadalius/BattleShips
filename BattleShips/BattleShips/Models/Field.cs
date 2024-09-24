acef6489d93b83fbf4725e7bf8872e00957b07e5
DESKTOP-89LA26K\vidma <vidmantaszuk@gmail.com>
2024-09-24 19:16:56 +0300

    field display component

---
+++ BattleShips/BattleShips/Models/Field.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleShips.Models
+{
+    public class Field
+    {
+        public string Name { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+
+    }
+}
