0633352d6e59f7c4d8816791d6d73ba9e7e1a3a5
taqq <serhij.kulyk@gmail.com>
2024-11-03 15:55:27 +0200

    Decorator modification
    Now the dragged ship highlights cells with different colors dependant on which type of ship it is.

--- BattleShips/BattleShips/Models/HighlightDecorator.cs
+++ BattleShips/BattleShips/Models/HighlightDecorator.cs
@@ -2,14 +2,31 @@
 {
     public class HighlightDecorator: FieldCellDecorator
     {
-        public HighlightDecorator(FieldCell cell) : base(cell) { }
+        private string _highlightColor;
+
+        public HighlightDecorator(FieldCell cell, Ship draggedShip) : base(cell)
+        {
+            _highlightColor = GetHighlightColor(draggedShip.ShipTypeID);
+        }
+
+        private string GetHighlightColor(int shipTypeId)
+        {
+            return shipTypeId switch
+            {
+                1 => "rgba(0, 255, 0, 0.5)",
+                2 => "rgba(255, 0, 0, 0.5)",
+                3 => "rgba(0, 0, 255, 0.5)",
+                4 => "rgba(255, 255, 0, 0.5)",
+                _ => "rgba(255, 255, 255, 0.5)" 
+            };
+        }
 
         public override string GetCellColorState(bool showShips)
         {
             if (!decoratedCell.IsShot && decoratedCell.CellShip == null)
             {
-                //hovered color
-                return "rgb(255, 255, 0)";
+                // Return the highlighted color instead of the default
+                return _highlightColor;
             }
             return base.GetCellColorState(showShips);
         }
