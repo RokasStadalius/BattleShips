c1756e2e77019b7225dc56c9ed0355da6086e47a
DESKTOP-89LA26K\vidma <vidmantaszuk@gmail.com>
2024-11-05 15:00:37 +0200

    more tests

---
+++ BattleShips/BattleShipsTestingProject/Modules/Objects/UserStateTests.cs
@@ -0,0 +1,36 @@
+﻿using BattleShips.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipsTestingProject.Modules.Objects
+{
+    public class UserStateTests
+    {
+        [Fact]
+        public void SetUser_UpdatesCurrentUser()
+        {
+            var userState = new UserState();
+            var user = new User("Testas");
+
+            userState.SetUser(user);
+
+            Assert.Equal(user, userState.CurrentUser);
+        }
+
+        [Fact]
+        public void SetUser_Notifies()
+        {
+            var userState = new UserState();
+            var user = new User("Testas");
+            bool eventFired = false;
+
+            userState.OnChange += () => eventFired = true;
+            userState.SetUser(user);
+
+            Assert.True(eventFired);
+        }
+    }
+}
---
+++ BattleShips/BattleShipsTestingProject/Modules/Objects/UserTests.cs
@@ -0,0 +1,42 @@
+﻿using BattleShips.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipsTestingProject.Modules.Objects
+{
+    public class UserTests
+    {
+        [Fact]
+        public void User_Constructor_SetsUserName()
+        {
+            string expectedUserName = "Testas";
+
+            var user = new User(expectedUserName);
+
+            Assert.Equal(expectedUserName, user.UserName);
+        }
+
+        [Fact]
+        public void User_Constructor_GeneratesUserId()
+        {
+            var user = new User("Testas");
+
+            string userId = user.UserId;
+
+            Assert.False(string.IsNullOrEmpty(userId));
+        }
+
+        [Fact]
+        public void User_Constructor_SetsUserFieldToNull()
+        {
+            var user = new User("Testas");
+
+            var userField = user.userField;
+
+            Assert.Null(userField);
+        }
+    }
+}
